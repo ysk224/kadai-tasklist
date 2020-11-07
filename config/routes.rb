02f0946c3f9b092ac860c42863f904e41155938a
yusuke.sasaki <superusk2013@gmail.com>
2020-11-07 11:23:04 +0000

    revised2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
-  get 'toppages/index'
   get 'signup', to: 'users#new'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
